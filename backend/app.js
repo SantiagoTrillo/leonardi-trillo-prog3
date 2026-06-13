75b062363e23accfef99f9908dc8f6b331e0cd3a
Santiago Trillo <santitrillo07@gmail.com>
2026-06-13 18:35:18 -0300

    feat/13-6/Estructura de rutas básica implementada
    Co-authored-by: Santiago Tomás Leonardi <st3101@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,11 @@ import ejs from "ejs"
 import cors from "cors"
 import express from "express"
 import path from "path"
-import {fileURLToPath} from "url"
-
-import {rutaProducto} from './api/routes/rutaProducto.js'
+import { fileURLToPath } from "url"
+import { rutaProducto } from './routes/api/productos.routes.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
 const app = express()
 const PUERTO = process.env.PORT
 
@@ -18,11 +16,12 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(cors());
 
 app.use("/api", rutaProducto);
+app.use("/admin")
 
-rutaProducto.get("/test", (req, res) => {
-    console.log("test");
-    res.send("test");
-});
+// rutaProducto.get("/test", (req, res) => {
+//     console.log("test");
+//     res.send("test");
+// });
 
 export default rutaProducto;
 
---
+++ backend/routes/api/productos.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const rutaApi = express.Router();
+
+rutaApi.get("/productos", (req, res) => {
+    res.json(productos)
+});
+
+export { rutaApi };
