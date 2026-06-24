52cb1610fcff8674ddfe7c2fc50e044d7d4b22a9
Santiago Trillo <santitrillo07@gmail.com>
2026-06-24 18:26:35 -0300

    style/24-6/Remoción de comentarios

--- frontend/bienvenida/bienvenida.js
+++ frontend/bienvenida/bienvenida.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Limpiar datos de la sesión anterior al ingresar a la pantalla de bienvenida
-    sessionStorage.removeItem("nombreCliente");
     sessionStorage.removeItem("carrito");
     sessionStorage.removeItem("ultimoTicket");
 
